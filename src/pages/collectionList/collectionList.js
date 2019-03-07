e8b36d90461cabd1432e236a6666a7f890ab63e1
bravo <2385097162@qq.com>
2019-03-07 14:59:26 +0800

    feat:歌单合集

---
+++ src/pages/collectionList/collectionList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class collectionList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+collectionList.propTypes = {
+
+};
+
+export default collectionList;
